0ef4e67359857587e013976a1bae4332d436daf6
Alex Samuel <alex@alexsamuel.net>
2019-09-01 16:53:37 -0400

    Refactor.

--- hexdump/src/main.rs
+++ hexdump/src/main.rs
@@ -46,12 +46,10 @@ fn parse_args(argv: env::Args) -> Args {
 }
 
 
-fn hexdump(args: Args) -> std::io::Result<()> {
-    let file = try!(File::open(args.path));
-    let mut buf_reader = BufReader::new(file);
+fn hexdump(reader: &mut BufReader<File>) {
     let mut buf: [u8; 8] = [0; 8];
     loop {
-        let num_read = buf_reader.read(&mut buf).unwrap();
+        let num_read = reader.read(&mut buf).unwrap();
         for i in 0 .. num_read {
             print!("{:02x} ", buf[i]);
         }
@@ -60,12 +58,14 @@ fn hexdump(args: Args) -> std::io::Result<()> {
             break
         }
     }
-    Ok(())
 }
 
 
 fn main() -> std::io::Result<()> {
     let args = parse_args(env::args());
-    hexdump(args)
+    let file = try!(File::open(args.path));
+    let mut buf_reader = BufReader::new(file);
+    hexdump(&mut buf_reader);
+    Ok(())
 }
 

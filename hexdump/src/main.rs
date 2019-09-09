150aa1be575aa6b8ad76e3bd11f2b9c9ead49b68
Alex Samuel <alex@alexsamuel.net>
2019-09-09 08:27:42 -0400

    Formatting.

--- hexdump/src/main.rs
+++ hexdump/src/main.rs
@@ -12,12 +12,10 @@ use std::iter::Iterator;
 use std::process;
 use std::vec::Vec;
 
-
 #[derive(Debug)]
 struct Args {
     path: String,
 }
-
 
 static USAGE: &'static str = "usage: hexdump [ OPTIONS ] PATH";
 
@@ -49,8 +47,7 @@ fn parse_args(argv: env::Args) -> Args {
     Args { path: path }
 }
 
-
-fn hexdump<I>(mut bytes: I, line_len: u16) 
+fn hexdump<I>(mut bytes: I, line_len: u16)
 where
     I: Iterator<Item = Result<u8, Error>>,
 {
@@ -59,14 +56,18 @@ where
     }
 
     fn render(c: u8) -> char {
-        if c < 32 || c > 126 { '\u{2022}' } else { c as char }
+        if c < 32 || c > 126 {
+            '\u{2022}'
+        } else {
+            c as char
+        }
     }
 
     let mut pos: u64 = 0;
     let mut done = false;
     let mut line: Vec<u8> = Vec::with_capacity(line_len as usize);
     while !done {
-        for line_pos in 0 .. line_len {
+        for line_pos in 0..line_len {
             if let Some(Ok(val)) = bytes.next() {
                 // Start of line: show position.
                 if line_pos == 0 {
@@ -85,7 +86,7 @@ where
         }
 
         // Pad out a partial line.
-        for _ in line.len() .. line_len as usize {
+        for _ in line.len()..line_len as usize {
             print!("   ");
         }
 
@@ -100,28 +101,31 @@ where
     }
 }
 
-
 const TIOCGWINSZ: libc::c_ulong = 0x40087468;
 
 #[repr(C)]
 struct winsize {
-    ws_row: libc::c_ushort, /* rows, in characters */
-    ws_col: libc::c_ushort, /* columns, in characters */
+    ws_row: libc::c_ushort,    /* rows, in characters */
+    ws_col: libc::c_ushort,    /* columns, in characters */
     ws_xpixel: libc::c_ushort, /* horizontal size, pixels */
-    ws_ypixel: libc::c_ushort /* vertical size, pixels */
+    ws_ypixel: libc::c_ushort, /* vertical size, pixels */
 }
 
 fn get_winsize() -> std::io::Result<winsize> {
-    let w = winsize { ws_row: 0, ws_col: 0, ws_xpixel: 0, ws_ypixel: 0 };
+    let w = winsize {
+        ws_row: 0,
+        ws_col: 0,
+        ws_xpixel: 0,
+        ws_ypixel: 0,
+    };
     match unsafe { libc::ioctl(libc::STDOUT_FILENO, TIOCGWINSZ, &w) } {
         0 => Ok(w),
-        _ => Err(std::io::Error::last_os_error())
+        _ => Err(std::io::Error::last_os_error()),
     }
 }
 
-
-fn main() -> std::io::Result<()> { 
-    // Rust programs ignore SIGPIPE by default.  Reenable the default 
+fn main() -> std::io::Result<()> {
+    // Rust programs ignore SIGPIPE by default.  Reenable the default
     // disposition (termination) so the program is killed if input or output
     // breaks.
     unsafe {
